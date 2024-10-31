8ddc33796218d9a2efd3722ba2fa3ee002e40a78
yo1 <yo1.cvetkov@gmail.com>
2024-10-31 10:23:17 +0100

    Prettier config file added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,14 @@
-import { authController } from "@/controllers/auth.controller";
-import { CreateUserDto } from "@/dto/createUser.dto";
-import { LoginUserDto } from "@/dto/loginUser.dto";
-import validateDto from "@/middlewares/validateDto.middleware";
-import { Router } from "express";
+import { authController } from '@/controllers/auth.controller';
+import { CreateUserDto } from '@/dto/createUser.dto';
+import { LoginUserDto } from '@/dto/loginUser.dto';
+import validateDto from '@/middlewares/validateDto.middleware';
+import { Router } from 'express';
 
 const router = Router();
 
-router.post("/login", validateDto(LoginUserDto), authController.login);
-router.get("/token", authController.token);
-router.delete("/logout", authController.logout);
-router.post("/register", validateDto(CreateUserDto), authController.register);
+router.post('/login', validateDto(LoginUserDto), authController.login);
+router.get('/token', authController.token);
+router.delete('/logout', authController.logout);
+router.post('/register', validateDto(CreateUserDto), authController.register);
 
 export default router;
